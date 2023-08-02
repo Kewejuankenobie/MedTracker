2b41cec82aacace1298c2c3aa7f003e744f63b07
Kewejuankenobie <97452684+Kewejuankenobie@users.noreply.github.com>
2023-08-02 09:27:53 -0400

    Some more text
    Listens for touching my name and redirects it to an inline function

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
+
+  console.log("App Runs");
+
+  //View (like div) holds stuff
+  //Safe area adds padding to top to ignore notch
+  //On Press listens for an event
+
+  const pressName = () =>
+    console.log("Hello World");
+
   return (
-    <View style={styles.container}>
-      <Text>Hello, Kiron</Text>
+    <SafeAreaView style={styles.container}>
+      <Text onPress={pressName}>Hello, Kiron</Text>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'dodgerblue',
   },
 });
