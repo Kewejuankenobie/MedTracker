d743580fb88ff6def810dcf378f6372ec74f38ad
Kewejuankenobie <97452684+Kewejuankenobie@users.noreply.github.com>
2023-08-11 10:45:14 -0400

    New required mechanics
    Can take a pill and refill a prescription

--- MedList.js
+++ MedList.js
@@ -35,22 +35,55 @@ export default class MedList extends Component {
         }
     }
 
+    takePill(k) {
+        //Function to take a pill
+        for (let i = 0; i < this.state.medList.length; i++) {
+            if (this.state.medList[i].key == k) {
+                let tmpList = this.state.medList;
+                if (tmpList[i].current == 0) {
+                    alert("Out of Pills")
+                    break;
+                } else {
+                    tmpList[i].current--;
+                    this.setState({medList: tmpList});
+                    break;
+                }
+            }
+        }
+    }
+
+    addPerscription(k) {
+        //Function to add an extra perscription of pills to current
+        for (let i = 0; i < this.state.medList.length; i++) {
+            if (this.state.medList[i].key == k) {
+                let tmpList = this.state.medList;
+                tmpList[i].current += tmpList[i].persc;
+                this.setState({medList: tmpList});
+                break;
+            }
+        }
+    }
+
     render() {
         return (
             <View>
+                <AddMedPage med = {this.addMed}/>
                 {
                     this.state.medList.map(item => (
                         <View style = {styles.medBox} key = {item.key}>
                             <Text>{item.name}</Text>
-                            <Text>Perscribed Amount: {item.persc}</Text>
-                            <Text>Current Amount: {item.current}</Text>
+                            <Text>Prescribed Amount: {item.persc}</Text>
+                            <Text>Remaining Amount: {item.current}</Text>
                             <Text>Dossage: {item.dosage}</Text>
-                            <Button title = "Remove"
+                            <Button title = "Take pill"
+                            onPress = {this.takePill.bind(this, item.key)}/>
+                            <Button title = "Add Perscription"
+                            onPress = {this.addPerscription.bind(this, item.key)}/>
+                            <Button title = "Remove Med"
                             onPress = {this.removeMed.bind(this, item.key)}/>
                         </View>
                     ))
                 }
-                <AddMedPage med = {this.addMed}/>
             </View>
         );
     }
